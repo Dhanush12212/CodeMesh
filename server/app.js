ee70dba51150bf49fd0e5c139b6409197454461e
Dhanaush12212 <dhanush12212@gmail.com>
2025-03-06 18:47:33 +0530

    Started Server

---
+++ server/app.js
@@ -0,0 +1,39 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express'; 
+import AuthRoute from './routes/AuthRoute.js';
+import cookieParser from 'cookie-parser';
+import connectDB from './db/connectDB.js';
+// import cors from 'cors'
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cookieParser)
+
+app.use('/api/Auth',AuthRoute);
+
+const startServer = async() => {
+    try {
+        await connectDB();
+        app.listen(PORT, '0.0.0.0', () => {
+            console.log(
+                JSON.stringify({message: `Server connected on port ${PORT}`, status: "success"})
+            );
+        });
+    } catch(error) {
+        console.error(
+            JSON.stringify({ 
+                message: "Failed to start server", 
+                status:"failure", 
+                error:error.message
+            })
+        );
+        process.exit(1); 
+    }
+};
+
+startServer();
